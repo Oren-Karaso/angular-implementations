d85a4888d43405f1d5988259453f61a1d895c429
Oren Karaso <orenkaraso@gmail.com>
2024-03-22 16:31:30 +0200

    MyAsync pipe

---
+++ angular-implementations/src/app/pipes/my-async.ts
@@ -0,0 +1,31 @@
+import { ChangeDetectorRef, OnDestroy, Pipe, PipeTransform } from "@angular/core";
+import { Observable, Subject, from, isObservable, takeUntil } from "rxjs";
+
+@Pipe({ name: 'myAsync', pure: false }) // Marking an unpure so the pipe will be triggered on every change detection cycle
+export class MyAsyncPipe implements PipeTransform, OnDestroy {
+    private _currentValue?: any;
+    private _destroy$ = new Subject<void>();
+
+    constructor(private readonly _ref: ChangeDetectorRef) { }
+
+    ngOnDestroy() {
+        this._destroy$.next();
+        this._destroy$.complete();
+    }
+
+    transform<T>(asyncDataSource$: Observable<T> | Promise<T>): T | undefined {
+        const data$ = isObservable(asyncDataSource$) ? asyncDataSource$ : from(asyncDataSource$); // Converting to observable in case of a promise input
+        data$.pipe(
+            takeUntil(this._destroy$) // onDestroy will next the subject and complete the data$ stream
+        ).subscribe({
+            next: value => {
+                this._currentValue = value;
+                this._ref.markForCheck();
+            },
+            error: error => {
+                console.error(error);
+            }
+        });
+        return this._currentValue; // Correct type will be infered
+    }
+}
